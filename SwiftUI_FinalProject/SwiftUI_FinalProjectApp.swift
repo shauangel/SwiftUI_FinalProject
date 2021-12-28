583138b53ed3955ef6728432a681f72dd055a93c
Angel Shau <shauangel2000@gmail.com>
2021-12-28 11:25:50 +0800

    Initial Commit

---
+++ SwiftUI_FinalProject/SwiftUI_FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FinalProjectApp.swift
+//  SwiftUI_FinalProject
+//
+//  Created by 邵安祺 on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
